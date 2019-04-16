5a532747c4bd1ac69fe2c2c8bf8227f2072fddca
arjaaam <majra_dreznjak@hotmail.com>
2019-04-16 16:10:37 +0200

    Ponovoooo

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ ActionMailer::Base.smtp_settings= {
   :domain => 'heroku.com',
   :authentication => :plain,
   :user_name => 'apikey',
-  :password => ['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',
+  :password =>'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',
   :enable_starttls_auto => true
 }
 
