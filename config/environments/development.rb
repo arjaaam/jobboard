a03c5cb7e396676b682d627aae99e1e9eefa6bbf
arjaaam <majra_dreznjak@hotmail.com>
2019-04-16 16:41:32 +0200

    isravit development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,9 +6,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_mailer.perform_deliveries = true
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.delivery_method= :test
+  config.action_mailer.delivery_method= :smtp
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -29,7 +30,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
