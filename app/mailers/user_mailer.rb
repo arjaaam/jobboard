b4500d4969d4e8dcd72b846811a2153a118db9ac
arjaaam <majra_dreznjak@hotmail.com>
2019-04-18 16:02:21 +0200

    Email izgled verzija3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,14 @@ class UserMailer < ApplicationMailer
   #layout 'mailer'
   def everyday_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Every day email')
+#    @category_idd=Category.find_by(id: params[@user.category_id])
+    Category.all.each do |kategori|
+      if (kategori.id == @user.category_id)
+          @company = Company.all
+          @job=Job.where(category_id: kategori.id).order(created_at: :desc)
+          mail(to: @user.email, subject: 'Every day email')
+      end
+    end
   end
 
 end
